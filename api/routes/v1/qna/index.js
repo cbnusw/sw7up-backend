2c4c9184b5aaf629cb018207f2a20094c8315e6d
kmahn <ahnmo777@gmail.com>
2021-04-17 10:25:53 +0900

    API 수정: 비로그인 qna 및 댓글 삭제를 post 메소드로 변경

--- api/routes/v1/qna/index.js
+++ api/routes/v1/qna/index.js
@@ -12,13 +12,13 @@ router.post('/', authenticate, controller.createQnA);
 router.post('/:id', authenticate, controller.getQnA);
 router.post('/:id/reply', authenticate, controller.addReply);
 router.post('/:id/password', authenticate, controller.checkPassword);
+router.post('/:id/remove', authenticate, controller.removeQnA);
+router.post('/:id/reply/:replyId/remove', controller.removeReply);
 
 router.put('/:id', authenticate, controller.updateQnA);
 router.put('/:id/reply/:replyId', authenticate, controller.updateReply);
 
 router.patch('/:id/confirm', ...hasPermission('qna'), controller.confirm);
-router.patch('/:id/remove', authenticate, controller.removeQnA);
-router.patch('/:id/reply/:replyId/remove', controller.removeReply);
 
 router.delete('/:id', ...hasPermission('qna'), controller.removeQnA);
 router.delete('/:id/reply/:replyId', isAuthenticated, controller.removeReply);

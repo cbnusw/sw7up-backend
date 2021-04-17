f0db6366e3b2ee2e0ab8cb513e8489370de8dc4e
kmahn <ahnmo777@gmail.com>
2021-04-17 16:07:50 +0900

    API 권한 수정: 사용자 관리 관련 권한 수정

--- api/routes/v1/user/index.js
+++ api/routes/v1/user/index.js
@@ -1,5 +1,5 @@
 const { Router } = require('express');
-const { hasPermission, hasSomePermissions, isAdmin } = require('../../../../shared/middlewares/auth');
+const { hasPermission, hasSomePermissions, isAdmin, isOperator } = require('../../../../shared/middlewares/auth');
 const controller = require('./controller');
 
 const router = Router();
@@ -14,20 +14,20 @@ router.get('/operator/:id', ...hasPermission('operator'), controller.getOperator
 router.post('/student', ...hasPermission('student'), controller.registerStudent);
 router.post('/staff', ...hasPermission('staff'), controller.registerStaff);
 router.post('/operator', ...hasPermission('operator'), controller.registerOperator);
-router.post('/:id/restore', ...isAdmin, controller.restore);
+router.post('/:id/restore', ...isOperator, controller.restore);
 
 router.put('/student/:id', ...hasPermission('student'), controller.updateStudent);
 router.put('/staff/:id', ...hasPermission('staff'), controller.updateStaff);
 router.put('/operator/:id', ...hasPermission('operator'), controller.updateOperator);
 
-router.patch('/:id/permissions', ...isAdmin, controller.setPermissions);
+router.patch('/:id/permissions', ...isOperator, controller.setPermissions);
 router.patch('/:id/admin/add', ...isAdmin, controller.addAdminRole);
 router.patch('/:id/admin/remove', ...isAdmin, controller.removeAdminRole);
-router.patch('/:id/operator/add', ...hasSomePermissions('student', 'staff', 'operator'), controller.addOperatorRole);
-router.patch('/:id/operator/remove', ...hasSomePermissions('student', 'staff', 'operator'), controller.removeOperatorRole);
+router.patch('/:id/operator/add', ...isAdmin, controller.addOperatorRole);
+router.patch('/:id/operator/remove', ...isAdmin, controller.removeOperatorRole);
 
 router.delete('/clear', ...isAdmin, controller.clear);
-router.delete('/:id', ...isAdmin, controller.removeUser);
+router.delete('/:id', ...isOperator, controller.removeUser);
 router.delete('/:id/clear', ...isAdmin, controller.clearUser);
 
 module.exports = router;

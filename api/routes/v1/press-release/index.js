30bb6b011e5cea9379ce6e8f2dbc6a94add0add4
kmahn <ahnmo777@gmail.com>
2021-04-17 09:57:37 +0900

    코드 수정: hasPermission 미들웨어 spread 연산 적용

--- api/routes/v1/press-release/index.js
+++ api/routes/v1/press-release/index.js
@@ -6,8 +6,8 @@ const router = Router();
 
 router.get('/', controller.getPressReleases);
 router.get('/:id', authenticate, controller.getPressRelease);
-router.post('/', hasPermission('press release'), controller.createPressRelease);
-router.put('/:id', hasPermission('press release'), controller.updatePressRelease);
-router.delete('/:id', hasPermission('press release'), controller.removePressRelease);
+router.post('/', ...hasPermission('press release'), controller.createPressRelease);
+router.put('/:id', ...hasPermission('press release'), controller.updatePressRelease);
+router.delete('/:id', ...hasPermission('press release'), controller.removePressRelease);
 
 module.exports = router;

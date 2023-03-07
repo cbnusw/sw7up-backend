d2454c0b768d8cad737bfd4fd482b671cb0cdaa4
kmahn <ahnmo777@gmail.com>
2023-03-07 10:51:53 +0900

    관리자 기능

--- code/routes/managements/index.js
+++ code/routes/managements/index.js
@@ -4,9 +4,9 @@ const controller = require('./controller');
 
 const router = Router();
 
-// router.get('/projects', isOperator, controller.getProjects);
-// router.get('/projects/download', isOperator, controller.downloadProjects);
-router.get('/projects', controller.getProjects);
-router.get('/projects/download', controller.downloadProjects);
+router.get('/projects', isOperator, controller.getProjects);
+router.get('/projects/download', isOperator, controller.downloadProjects);
+// router.get('/projects', controller.getProjects);
+// router.get('/projects/download', controller.downloadProjects);
 
 module.exports = router;
